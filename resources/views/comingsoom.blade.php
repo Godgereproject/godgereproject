ac0db624220e86fe37b963c75e95369645999c10
Godgereproject <admin@godgereproject.com>
2022-08-23 23:24:14 +0100

    first commit

---
+++ resources/views/comingsoom.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!--====== Title ======-->
+    <title>God Gère Project || Freelancing</title>
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png" />
+    <!--====== Animate CSS ======-->
+    <link rel="stylesheet" href="assets/css/animate.min.css" />
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+    <!--====== OWl carousel ======-->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+
+<body>
+    <!--====== Preloader ======-->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="cssload-container">
+            <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
+        </div>
+    </div>
+
+    <div class="page-wrapper project-four image-version">
+        <div class="page-content">
+            <div class="container">
+                <div class="row no-gutters">
+                    <div class="col-lg-4 offset-lg-4">
+                        <div class="coming-soon-title">
+                            <h3>Coming Soon</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        <h1 class="title">
+                            GGP <br> FREELANCING
+                        </h1>
+                        <div class="counter-wrap" id="countdownTwo" data-countdown="2022/10/02">
+                        </div>
+                        <a href="#subscibeModal" class="main-btn" data-toggle="modal">Nous écrire</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--====== Modal ======-->
+        <div class="modal fade subscibe-modal modal-two" id="subscibeModal" role="dialog">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <span>Restez informé</span>
+                        <h2>Nous arrivons <br> Bientôt</h2>
+                        <p>Abonnez-vous à notre newsletter et nous vous enverrons une notification concernant le lancement de la plateforme <b>God Gère Project</b></p>
+                        <form action="#">
+                            <input type="email" placeholder="Votre adresse email*">
+                            <button type="submit">Envoyer</button>
+                        </form>
+                        <a href="#" class="close-modal" data-dismiss="modal"><i class="far fa-times"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <!--====== Countdown js ======-->
+    <script src="assets/js/jquery.countdown.js"></script>
+    <!--====== Particles js ======-->
+    <script src="assets/js/particles.min.js"></script>
+    <!--====== wow js ======-->
+    <script src="assets/js/wow.min.js"></script>
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+</body>
+
+</html>	
